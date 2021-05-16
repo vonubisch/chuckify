2c359d6ca4d41cd8104bf8e00241e5bac4589336
VULAPTOP\Bjorn <bs@vonubisch.com>
2021-05-16 20:40:21 +0200

    CSS Structure changed, moved strictmode to App component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import './css/tailwind.css';
+
 import App from './components/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('app'));
