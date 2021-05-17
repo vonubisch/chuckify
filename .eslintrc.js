fedd7f64f5422cfa0bbab9dac978981fecf74859
VULAPTOP\Bjorn <bs@vonubisch.com>
2021-05-17 15:03:03 +0200

    Installation of Cypress plugin for ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
-        "plugin:react/recommended"
+        "plugin:react/recommended",
+        "plugin:cypress/recommended"
     ],
     "parserOptions": {
         "ecmaFeatures": {
@@ -15,7 +16,8 @@ module.exports = {
         "sourceType": "module"
     },
     "plugins": [
-        "react"
+        "react",
+        "cypress"
     ],
     "rules": {
     },
